39d7e8af102dc82d1d0095e6f89249cf37870e21
Vrahimis <mvrahimis@iona.edu>
2025-09-17 16:28:06 -0400

    testing git

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express")
+const mongoose = require("mongoose")
+
+
+const app = express()
+app.use(express.json())
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("connect")).catch((err) => console.error(err)) 
+
+app.get("/", (req, res) => {
+    console.log("base route hit")
+    res.send("ice cream install")
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log("server started")
+})
+// mjnw1dcheyZBslfn
